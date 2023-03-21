9e3336424fed99153123ad7ca6f39dcde5e680f6
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-03-21 21:32:05 +0100

    Fixed cards alignment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,7 +49,7 @@ const About = () => (
          to bring your ideas to life!
       </motion.p>
 
-      <div className='flex flex-wrap gap-10 mt-20'>
+      <div className='flex flex-wrap justify-center gap-10 mt-20'>
          {services.map((service, index) => (
             <ServiceCard key={service.title} index={index} {...service} />
          ))}
