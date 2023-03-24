81495e5fe35f8ccb2ebef7a6e282e77c6406e681
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-03-24 23:10:33 +0100

    Updated CanvasLoader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,14 +4,23 @@ const Loader = () => {
    const { progress } = useProgress()
 
    return (
-      <Html>
+      <Html
+         as='div'
+         center
+         style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+         }}
+      >
          <span className='canvas-loader'></span>
          <p
             style={{
                fontSize: 14,
                color: '#f1f1f1',
                fontWeight: 800,
-               marginTop: 40
+               marginTop: 40,
             }}
          >
             {progress.toFixed(2)}%
