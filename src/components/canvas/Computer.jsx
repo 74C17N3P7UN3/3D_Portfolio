c98f6dfe86be3a41e9ac766167ec9e720063c7d1
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-03-24 19:35:25 +0100

    Disabled panning on canvas

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -63,6 +63,7 @@ const ComputerCanvas = () => {
          <Suspense fallback={<CanvasLoader />}>
             <OrbitControls
                enableZoom={false}
+               enablePan={false}
                maxPolarAngle={Math.PI / 2}
                minPolarAngle={Math.PI / 2}
             />
